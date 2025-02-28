f59f757b563c58621b8d830124a2531159efb7fe
Sahan Udara <sahanherath444@gmail.com>
2025-02-28 16:50:12 +0530

    Prod Content Update and LAyout Add -Sahan Prod-V2

--- src/app/page.js
+++ src/app/page.js
@@ -82,7 +82,7 @@ export default function Home() {
                 <AboutMe/>
                 <Services/>
                 <Skills/>
-                <Testimonial/>
+                {/*<Testimonial/>*/}
             </main>
 
         </div>
--- src/components/WorkSection.js
+++ src/components/WorkSection.js
@@ -14,24 +14,24 @@ const WorkSection = () => {
                            src={"https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
                            alt={"poster"} width={"450"} height={"450"}/>
                 </div>
-                <div className="work__card    md:basis-[450px] lg:basis-[350px] xl:basic-[450px]   p-4 m-5 ">
-                    <Image className={"h-full"} style={{objectFit: "cover"}} src={"/images/diana.jpeg"} alt={"poster"}
+                <div className="work__card  aspect-[1/1]    md:basis-[450px] lg:basis-[350px] xl:basic-[450px]   p-4 m-5 ">
+                    <Image className={"h-full"} style={{objectFit: "cover"}} src={"https://images.unsplash.com/photo-1618477388954-7852f32655ec?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt={"poster"}
                            width={"450"} height={"350"}/>
                 </div>
-                <div className="work__card   md:basis-[450px]  lg:basis-[350px] xl:basic-[450px]   p-4 m-5 ">
-                    <Image className={"h-full"} style={{objectFit: "cover"}} src={"/images/diana.jpeg"} alt={"poster"}
+                <div className="work__card aspect-[1/1]    md:basis-[450px]  lg:basis-[350px] xl:basic-[450px]   p-4 m-5 ">
+                    <Image className={"h-full"} style={{objectFit: "cover"}} src={"https://images.unsplash.com/photo-1581472723648-909f4851d4ae?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt={"poster"}
                            width={"450"} height={"350"}/>
                 </div>
-                <div className="work__card   md:basis-[450px] lg:basis-[350px] xl:basic-[450px]  p-4 m-5 ">
-                    <Image className={"h-full"} style={{objectFit: "cover"}} src={"/images/diana.jpeg"} alt={"poster"}
+                <div className="work__card aspect-[1/1]    md:basis-[450px] lg:basis-[350px] xl:basic-[450px]  p-4 m-5 ">
+                    <Image className={"h-full"} style={{objectFit: "cover"}} src={"https://images.unsplash.com/photo-1573166801077-d98391a43199?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt={"poster"}
                            width={"450"} height={"350"}/>
                 </div>
-                <div className="work__card   md:basis-[450px] lg:basis-[350px] xl:basic-[450px] p-4 m-5 ">
-                    <Image className={"h-full"} style={{objectFit: "cover"}} src={"/images/diana.jpeg"} alt={"poster"}
+                <div className="work__card aspect-[1/1]    md:basis-[450px] lg:basis-[350px] xl:basic-[450px] p-4 m-5 ">
+                    <Image className={"h-full"} style={{objectFit: "cover"}} src={"https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=2015&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt={"poster"}
                            width={"450"} height={"350"}/>
                 </div>
-                <div className="work__card   md:basis-[450px]  lg:basis-[350px] xl:basic-[450px]   p-4  m-5 ">
-                    <Image className={"h-full"} style={{objectFit: "cover"}} src={"/images/diana.jpeg"} alt={"poster"}
+                <div className="work__card aspect-[1/1]    md:basis-[450px]  lg:basis-[350px] xl:basic-[450px]   p-4  m-5 ">
+                    <Image className={"h-full"} style={{objectFit: "cover"}} src={"https://images.unsplash.com/photo-1600132806370-bf17e65e942f?q=80&w=1988&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt={"poster"}
                            width={"450"} height={"350"}/>
                 </div>
             </div>
